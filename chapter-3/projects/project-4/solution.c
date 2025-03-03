36442e7133dab94bd17a98df8dd3662c7075a1b8
Ivan Khimich <himivan@gmail.com>
2025-03-03 20:45:16 +0300

    add chapter 3 project 4 solution

---
+++ chapter-3/projects/project-4/solution.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int area_code, phone_prefix, line_number;
+
+    printf("Enter phone number [(xxx) xxx-xxxx]: ");
+    scanf("(%d) %d-%d", &area_code, &phone_prefix, &line_number);
+
+    printf("You entered %d.%d.%d\n", area_code, phone_prefix, line_number);
+
+    return 0;
+}
